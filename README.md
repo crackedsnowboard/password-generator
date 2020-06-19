be83648b1f62ad4b79871f566b82ff7db964c1d7
Prince of Kochi <mathenjs@gmail.com>
2020-06-18 20:52:56 -0700

    Final draft of README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# password-generator
-Password generator using javascript
+# Password Generator
+This project was oriented around creating a randomly generated password based on user selected criteria. I primarily utilized Javascript to create the functions and logic.
+
+## User Password Criteria
+Upon arrival to the page, the user is presented with prompts to stipulate the parameters of their password. The criteria are as follows
+
+* Length of 8-128 characters
+* At least one of lowercase letter, uppercase letters, numbers, and special characters
+* The input features will be verified for the user and the length will be checked for accuracy
+
+## Code Strategy
+I used ```confirm``` to store the user answers as boolean values and used ```if``` statements then to confirm those choices back to the user in ```alerts```. If all the boolean were false then I alerted the user to please select one character class. 
+
+### Separate Functions
+Functions were created to generate random number which were used to index arrays of the character types with the results pushed to an empty string, globally defined. This required heavy use of the ```math.random``` method. 
+```
+Math.floor(Math.random() * 10); - for a random integer 0-9
+Math.floor(Math.random() * 26); - for a random integer 0-25 to index an array of the alphabet
+```
+#### Struggles
+
+I could not get the output to write to the webpage. Except for an undefined error. I tried to console.log my function result without results. Will have to try and rethink the functions I created and how they pass their results back and forth. 
+
+
+#### Author Links
+[LinkedIn](linkedin.com/in/joel-mathen/)
+[GitHub](https://github.com/crackedsnowboard)
+
--- script.js
+++ script.js
@@ -13,16 +13,38 @@ var userLength;
 var passwordSecret = [];
 var randomNum;
 var randomFunction = [randomNumber, randomSpecial, randomLower, randomUpper];
-var passwordStore = ""
+var passwordStore = "";
 var userLength;
 
 // Write password to the #password input
-function writePassword(password) {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
+// Function to generate password
+function generatePassword(length) {
+  for (i = 8; i <= userLength; i++) {
+    
+    if (lowerChoice === true) {
+      randomLower();
+    }
+    if (upperChoice === true) {
+      randomUpper();
+      i++
+    }
+    if (specialChoice === true) {
+      randomSpecial();
+      i++
+    }
+    if (numberChoice === true) {
+      randomNumber();
+      i++
+    }
+  }
+}
+// Function to confirm use choices
 function confirmChoices() {
   var userLength = parseInt(prompt("Please select the length of your password. Please choose a number between 8 - 128"));
   if ((userLength > 8) && (userLength <= 128)) {
@@ -32,58 +54,52 @@ function confirmChoices() {
     alert("You have not selected a number between 8 - 128.");
     return
   }
+  var lowerChoice = confirm("Do you want lower case letter as part of your password?");
+  var upperChoice = confirm("Do you want upper case letters as part of your password?");
+  var numberChoice = confirm("Do you want numbers as part of your password?");
+  var specialChoice = confirm("Do you want special characters as part of your password?");
 
-  lowerChoice.push(confirm("Do you want lower case letter as part of your password?"));
-  upperChoice.push(confirm("Do you want upper case letters as part of your password?"));
-  numberChoice.push(confirm("Do you want numbers as part of your password?"));
-  specialChoice.push(confirm("Do you want special characters as part of your password?"));
+  if (lowerChoice === true) {
+    alert("You have choosen lowercase letters");
+  }
+  if (upperChoice === true) {
+    alert("You have choosen uppercase letters");
+  }
+  if (numberChoice === true) {
+    alert("You have choosen numbers");
+  }
+  if (specialChoice === true) {
+    alert("You have choosen special characters");
+  }
+  if (lowerChoice && upperChoice && numberChoice && specialChoice === false) {
+    alert("Please select at least one character type");
+  }
 }
-
+// Function to get a random number and push choosen number to a global string
 function randomNumber() {
   var randomNum = Math.floor(Math.random() * 10);
   passwordStore.push(randomNum);
 }
 
+// Function to get a random lower letter and push the letter to a global string
 function randomLower() {
   var randomLowerLetter = Math.floor(Math.random() * 26);
   passwordStore.push(arrLowerLetter[randomLowerLetter]);
 }
 
+//  Function to get a random uppercase letter and push to a global string
 function randomUpper() {
   var randomUpperLetter = Math.floor(Math.random() * 26);
   passwordStore.push(arrUpperLetter[randomUpperLetter]);
 }
-
+// Function to get a random special character from an array of special character
 function randomSpecial() {
   var randomSpecialLetter = Math.floor(Math.random() * 12);
   passwordStore.push(arrSpecialCharacter[randomSpecialLetter]);
 }
-
-function generatePassword() {
-  for (i = 8; i <= userLength; i++) {
-    if (lowerChoice) {
-      randomLower();
-    }
-    if (upperChoice) {
-      randomUpper();
-      i++
-    }
-    if (specialChoice) {
-      randomSpecial();
-      i++
-    }
-    if (numberChoice) {
-      randomNumber();
-      i++
-    }
-  }
-  return passwordStore;
-}
-
-
+generatePassword(userLength);
 confirmChoices();
-generatePassword();
-
+writePassword();
 
 generateBtn.addEventListener("click", writePassword);
 
