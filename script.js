4f832a6699aba51c9b18c744aa52fe8eee32fad2
Prince of Kochi <mathenjs@gmail.com>
2020-06-18 19:28:06 -0700

    Set up mulitple function to separate the work of the program

--- script.js
+++ script.js
@@ -5,57 +5,85 @@ var generateBtn = document.querySelector("#generate");
 var arrLowerLetter = ["abcdefghijklmnopqrstuvwxyz"];
 var arrUpperLetter = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var arrSpecialCharacter = ["!@#$%^&*()"];
+var lowerChoice;
+var upperChoice;
+var numberChoice;
+var specialChoice;
+var userLength;
+var passwordSecret = [];
+var randomNum;
+var randomFunction = [randomNumber, randomSpecial, randomLower, randomUpper];
+var passwordStore = ""
+var userLength;
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(password) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  // Object. values() method is used to return an array whose elements are the enumerable property values found on the object. 
-// The ordering of the properties is the same as that given by the object manually is a loop is applied to the properties.
   passwordText.value = password;
 }
 
-function generatePassword() {
+function confirmChoices() {
   var userLength = parseInt(prompt("Please select the length of your password. Please choose a number between 8 - 128"));
-  if ((userLength > 8) && (userLength < 128)) {
-    alert("User password length: " + userLength);
+  if ((userLength > 8) && (userLength <= 128)) {
+    alert("Your chosen password length is: " + userLength);
   }
   else {
     alert("You have not selected a number between 8 - 128.");
+    return
   }
 
-  var lowerChoice = confirm("Do you want lower case letter as part of your password?");
-  var upperChoice = confirm("Do you want upper case letters as part of your password?");
-  var numberChoice = confirm("Do you want numbers as part of your password?");
-  var specialChoice = confirm("Do you want special characters as part of your password?");
-
-  
-  for (i = userLength; i <= 128; i++) {
-    var passwordSecret = [];
-    if (numberChoice) {
-      var randomNum = Math.floor(Math.random() * 10);
-      passwordSecret.push(randomNum); 
-    }
-    else if (lowerChoice) {
-      var randomLowerLetter = Math.floor(Math.random() * 26);
-      passwordSecret.push(arrLowerLetter[randomLowerLetter]); 
-    }
-    else if (upperChoice) {
-      var randomUpperLetter = Math.floor(Math.random() * 26);
-      passwordSecret.push(arrUpperLetter[randomLowerLetter]);
-    }
-    else if (specialChoice) {
-      var randomSpecialLetter = Math.floor(Math.random() * 10);
-      passwordSecret.push(arrSpecialCharacter[randomLowerLetter]);
-    }
-  }
-  return passwordSecret;
+  lowerChoice.push(confirm("Do you want lower case letter as part of your password?"));
+  upperChoice.push(confirm("Do you want upper case letters as part of your password?"));
+  numberChoice.push(confirm("Do you want numbers as part of your password?"));
+  specialChoice.push(confirm("Do you want special characters as part of your password?"));
 }
 
-writePassword();
+function randomNumber() {
+  var randomNum = Math.floor(Math.random() * 10);
+  passwordStore.push(randomNum);
+}
+
+function randomLower() {
+  var randomLowerLetter = Math.floor(Math.random() * 26);
+  passwordStore.push(arrLowerLetter[randomLowerLetter]);
+}
+
+function randomUpper() {
+  var randomUpperLetter = Math.floor(Math.random() * 26);
+  passwordStore.push(arrUpperLetter[randomUpperLetter]);
+}
+
+function randomSpecial() {
+  var randomSpecialLetter = Math.floor(Math.random() * 12);
+  passwordStore.push(arrSpecialCharacter[randomSpecialLetter]);
+}
+
+function generatePassword() {
+  for (i = 8; i <= userLength; i++) {
+    if (lowerChoice) {
+      randomLower();
+    }
+    if (upperChoice) {
+      randomUpper();
+      i++
+    }
+    if (specialChoice) {
+      randomSpecial();
+      i++
+    }
+    if (numberChoice) {
+      randomNumber();
+      i++
+    }
+  }
+  return passwordStore;
+}
+
+
+confirmChoices();
+generatePassword();
 
 
 generateBtn.addEventListener("click", writePassword);
 
-  
